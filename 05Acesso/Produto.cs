87d43851c1aba47deb3fbf44b81eead9c359f776
Nathaline Santos <nathalinep@gmail.com>
2026-05-19 09:41:10 -0300

    Acesso

---
+++ 05Acesso/Produto.cs
@@ -0,0 +1,14 @@
+class Produto
+{
+    /*
+        * Modificadores de acesso:
+        public    -> Atributos e Metodos visiveis em qualquer classe
+        private   -> Atributos e Metodos visiveis apenas na classe onde foi criado
+        protected -> Atributos e Metodos visiveis em classes onde são criados ou herdados (classes e subclasses)
+    */
+
+    public string nome;
+
+    private double valor;
+
+}
---
+++ 05Acesso/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05Acesso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Produto p = new Produto();
+            p.nome = "Coca-cola";
+            //p.valor = 5.00; // Não é possível acessar o atributo valor, pois ele é privado
+        }
+    }
+
+}
