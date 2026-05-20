27930b28b2a1b311309975ef6383ecdeea6a11bd
Nathaline Santos <nathalinep@gmail.com>
2026-05-20 11:04:41 -0300

    Estatico

---
+++ 12Estatico/Exemplo.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Exemplo
+{
+    
+    // Atributo Estatico
+    public static string nome = "Maria";
+
+    // Metodo Estatico
+    public static void soma(int n1, int n2)
+    {
+        Console.WriteLine("Soma: " + (n1 + n2));
+    }
+
+}
